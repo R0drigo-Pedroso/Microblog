b9286011a17fcc10f788365d939d437ac7bc5b1e
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-08-24 08:50:25 -0300

    atualizar

--- src/Banco.php
+++ src/Banco.php
@@ -4,9 +4,9 @@ use PDO, Exception;
 
 abstract class Banco {
     private static string $servidor = "localhost";
-    private static string $usuario = "root";
-    private static string $senha = "";
-    private static string $banco = "microblog_rodrigo";
+    private static string $usuario = "suniow89_rodrigo";
+    private static string $senha = "Rp582700+";
+    private static string $banco = "suniow89_microblog_rodrigo";
     private static PDO $conexao; 
 
     public static function conecta():PDO {
